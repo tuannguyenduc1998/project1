e53b108e95a7643a3020d1cd6a28ba09b9585c55
quyendangbys <quyen.dang@bys.com.vn>
2020-10-09 15:45:45 +0700

    - fix UI

--- src/app/employee/form-employee/form-employee.component.ts
+++ src/app/employee/form-employee/form-employee.component.ts
@@ -8,30 +8,32 @@ import { MyserviceService } from 'src/app/myservice.service';
   templateUrl: './form-employee.component.html',
   styleUrls: ['./form-employee.component.scss']
 })
-export class FormEmployeeComponent implements OnInit , OnDestroy{
+export class FormEmployeeComponent implements OnInit, OnDestroy {
   constructor(private myserviceService: MyserviceService) { }
   @Input() age;
   sub: Subscription;
   status = status;
   model = new User();
-  url = './assets/icon.jpg';
+  url = './assets/images.png';
+  isSubmit: boolean;
   // tslint:disable-next-line: no-output-on-prefix
   @Output() onChanges = new EventEmitter<number>();
 
   ngOnInit(): void {
-    this.sub = this.myserviceService.age.subscribe( data => {
+    this.model.name = 'Quyền';
+    this.sub = this.myserviceService.age.subscribe(data => {
       this.age = data;
     });
     console.log('Quan sát');
   }
   // tslint:disable-next-line: typedef
-  onChangeplus(){
+  onChangeplus() {
     this.myserviceService.age.next(this.myserviceService.age.value + 1);
     this.onChanges.emit(this.age);
   }
 
   // tslint:disable-next-line: typedef
-  onChangesub(){
+  onChangesub() {
     this.myserviceService.age.next(this.myserviceService.age.value - 1);
     this.onChanges.emit(this.age);
   }
@@ -39,20 +41,20 @@ export class FormEmployeeComponent implements OnInit , OnDestroy{
     this.sub.unsubscribe();
   }
   // tslint:disable-next-line: typedef
-  onSubmit(form){
-    if (this.model.name !== undefined || this.model.email !== undefined)
-    {
+  onSubmit(form) {
+    this.isSubmit = true;
+    if (this.model.name !== undefined || this.model.email !== undefined) {
       console.log(form);
     }
     else { console.log('Tên nhân viên hoặc email đang không có dữ liệu'); }
   }
   // tslint:disable-next-line: typedef
-  onReset(form){
-     console.log(form.reset(form));
+  onReset(form) {
+    console.log(form.reset(form));
   }
   // tslint:disable-next-line: typedef
-  onSelectFile(event){
-    if (event.target.files){
+  onSelectFile(event) {
+    if (event.target.files) {
       const reader = new FileReader();
       reader.readAsDataURL(event.target.files[0]);
       // tslint:disable-next-line: no-shadowed-variable
